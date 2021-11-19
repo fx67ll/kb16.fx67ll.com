5874941dd6d040f767f5d3f4d18fe1fd5e300ce2
fx67ll <2717745014@qq.com>
2021-11-19 12:02:37 +0800

    new version

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # kb16.fx67ll.com
 A Funny Project For Happy!
 
+## 0.1.2.20211119  
+* add failure days  
+* add color picker function  
+
 ## 0.1.1.20211103  
 * fix apple browser `new Date('yyyy-mm-dd hh:mm:ss')` NaN bug  
 * fix wrong daycounter bug  
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,7 +1,30 @@
 <template>
-	<div class="fx67ll-box">
-		<div class="fx67ll-tip">☠️这是学习的第{{ studentDays }}天，恭喜你距离毕业还有😅</div>
+	<div id="fx67ll-kb16" class="fx67ll-box">
+		<div id="fx67ll-kb16-title" class="fx67ll-tip">☠️这是学习的第{{ studentDays }}天，恭喜你距离毕业还有😅</div>
 		<div class="fx67ll-clock"></div>
+		<div class="fx67ll-footer">
+			Designed & Powered by
+			<a href="https://fx67ll.com" target="_blank">fx67ll</a>
+			&#12288; Copyright© 2018-{{ this.year }}&#12288;
+			<a href="https://beian.miit.gov.cn/#/Integrated/index" target="_blank">皖ICP备18017174号-2</a>
+		</div>
+		<el-dialog title="属性设置" :visible.sync="dialogVisible" width="200px">
+			<div class="color-box">
+				<div class="color-text">修改背景颜色：</div>
+				<div class="color-item">
+					<el-color-picker v-model="viewBgColor" @change="changeBgColor"></el-color-picker>
+				</div>
+			</div>
+			<div class="color-box">
+				<div class="color-text">修改标题颜色：</div>
+				<div class="color-item">
+					<el-color-picker v-model="viewTiColor" @change="changeTiColor"></el-color-picker>
+				</div>
+			</div>
+			<span slot="footer" class="dialog-footer">
+				<el-button type="primary" @click="closeDialog">确 定</el-button>
+			</span>
+		</el-dialog>
 	</div>
 </template>
 
@@ -16,15 +39,57 @@
 				studentDays: 0,
 				chnNumChar: ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"],
 				chnUnitSection: ["", "万", "亿", "万亿", "亿亿"],
-				chnUnitChar: ["", "十", "百", "千"]
+				chnUnitChar: ["", "十", "百", "千"],
+				year: moment().format('YYYY'),
+				dialogVisible: false,
+				viewBgColor: '#ffffff',
+				viewTiColor: '#FF0000'
 			};
 		},
 		mounted() {
 			this.initStuDays();
 			this.initClock();
+			this.consoleTips();
 			this.consoleMyDays();
+			this.consoleFailDays();
+			this.listenKeys();
 		},
 		methods: {
+			// 关闭面板
+			closeDialog() {
+				this.dialogVisible = false;
+			},
+			// 修改标题颜色
+			changeTiColor() {
+				document.getElementById('fx67ll-kb16-title').style.setProperty('color', this.viewTiColor);
+			},
+			// 修改背景颜色
+			changeBgColor() {
+				document.getElementById('fx67ll-kb16').style.setProperty('background-color', this.viewBgColor);
+			},
+			// 监听组合键 Ctrl + L
+			listenKeys() {
+				let self = this;
+				document.onkeydown = function(event) {
+					var e = event || window.event || arguments.callee.caller.arguments[0];
+					if (e.ctrlKey && e.keyCode === 76) {
+						self.dialogVisible = true;
+					}
+				};
+			},
+			// 提示
+			consoleTips() {
+				console.log('---------------Tips---------------');
+				console.log('按下 Ctrl + L 可设置部分属性！');
+				console.log('----------------------------------');
+			},
+			// 失败日
+			consoleFailDays() {
+				let failDays = moment(moment('20211113').format('YYYY-MM-DD')).diff(moment('20210823').format(
+						'YYYY-MM-DD'),
+					'day');
+				console.log('fx67ll已于2021年11月13日获得失败，持续时间仅' + failDays + '天');
+			},
 			// 输出彩蛋
 			consoleMyDays() {
 				let myDays = moment(moment('2022-03-31').format('YYYY-MM-DD')).diff(moment().format('YYYY-MM-DD'),
@@ -118,12 +183,25 @@
 		font-size: 2vw;
 		left: 6.5vw;
 	}
+
+	.flip-clock-single {
+		left: 6.5vw;
+	}
+
+	.flip-clock-double {
+		left: 6.5vw;
+	}
+
+	.flip-clock- {
+		left: 6.5vw;
+	}
 </style>
 <style lang="less" scoped="scoped">
 	.fx67ll-box {
 		width: 100%;
 		height: 100%;
 		overflow: hidden;
+		color: #ffffff;
 		.ban-user-select();
 
 		.fx67ll-tip {
@@ -140,6 +218,26 @@
 			position: relative;
 			top: 60%;
 			margin-top: -10vw;
+		}
+
+		.fx67ll-footer {
+			width: 100%;
+			padding: 20px 0;
+			text-align: center;
+			position: absolute;
+			bottom: 0;
+			user-select: text;
+		}
+
+		.color-box {
+			width: 100%;
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+
+			.color-text {
+				font-size: 16px;
+			}
 		}
 	}
 </style>
